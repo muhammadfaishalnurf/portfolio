81d1bb065d8077efe6d0b71a0a02d6cf9e9e7176
mhmmdalif <155944019+mhmmdalif@users.noreply.github.com>
2024-03-01 15:12:37 +0700

    new alif

--- Faishal/src/pages/index.js
+++ Faishal/src/pages/index.js
@@ -55,7 +55,7 @@ export default function Home() {
         </Layout>
 
         <HireMe />
-    <div className="absolute right-8 bottom-6 inline-block w-24">
+    <div className="absolute right-8 bottom-8 inline-block w-24">
       <Image src={lightBulb} alt="Codebucks" className="w-full h-auto" />
     </div>
       </main>
